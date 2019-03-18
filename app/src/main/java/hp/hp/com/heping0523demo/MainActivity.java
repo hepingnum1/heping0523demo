b36aa4fd62e98c2c74b94472ff21340cd34528fd
heping <heping@dianzhong.com>
2019-03-18 11:38:52 +0800

    ddd

--- app/src/main/java/hp/hp/com/heping0523demo/MainActivity.java
+++ app/src/main/java/hp/hp/com/heping0523demo/MainActivity.java
@@ -3,6 +3,9 @@ package hp.hp.com.heping0523demo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * 贺平 0318分支 1
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
